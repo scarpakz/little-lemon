07ba59ac5a22b8b36323ba5deb8e1811c8231a68
Jason Javier <jasonjjavier@gmail.com>
2024-08-27 12:48:18 -0300

    added header infos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 
 function App() {
   return (
     <>
-      <div>
-        Homepage
-      </div>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Nav from './Nav'
+function Header() {
+    return(
+        <header>
+            <div>BRAND HERE</div>
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
