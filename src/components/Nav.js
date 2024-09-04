6952eac2cc1a873bb9b698a52fae595a3f1d74ca
Jason Javier <jasonjjavier@gmail.com>
2024-09-03 22:06:11 -0300

    added styling on header and footer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,9 @@ function Nav() {
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
+                <li><a href="/about">Reservation</a></li>
                 <li><a href="/contact">Contact</a></li>
-                <li><a href="/services">Services</a></li>
+                <li><a href="/services">About</a></li>
             </ul>
         </nav>
     ) 
